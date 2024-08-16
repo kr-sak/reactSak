ea2a15eb26d85858ddb46df818fe65a7e8df0e29
Do-chill <kr.gmlee@gmail.com>
2024-08-16 17:03:34 +0900

    aws

--- ReactSpring/src/main/frontend/src/App.js
+++ ReactSpring/src/main/frontend/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	const [posts, setPosts] = useState([]);
+
+	useEffect (() => {
+		// axios({
+		// 	method: 'GET',
+		// 	url:'https://jsonplaceholder.typicode.com/photos'
+		// }).then(response => setPosts(response.data))
+
+		// axios.get('https://jsonplaceholder.typicode.com/photos')
+		// 	 .then(response => setPosts(response.data))
+		const fetchData = async () => {
+		try {
+			const response = await axios.get('https://jsonplaceholder.typicode.com/photos');
+			setPosts(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	fetchData().catch(console.error)
+	})
+
+	return (
+		<ul>
+			{posts.map(post => (
+				<li key = {post.id}>
+				<div>{post.title}</div>
+				<div><img src = {post.thumbnailUrl}></img></div>
+				</li>
+			))}
+		</ul>
+	)
 }
 
 export default App;
